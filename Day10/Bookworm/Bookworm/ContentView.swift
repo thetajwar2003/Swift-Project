dcf939f43d399a3f037525aee17ca89f3574e492
thetajwar2003 <rahmant7@bxscience.edu>
2020-12-31 23:55:27 -0500

    Bookworm

---
+++ Day10/Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Tajwar Rahman on 12/31/20.
+//  Copyright © 2020 Tajwar Rahman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(books, id: \.self) { book in
+                    NavigationLink(destination: Text(book.title ?? "Unknown title")){
+                        EmojiRatingView(rating: book.rating).font(.largeTitle)
+                        VStack(alignment: .leading) {
+                            Text(book.title ?? "Unknown title").font(.headline)
+                            Text(book.author ?? "Unknown author").foregroundColor(.secondary)
+                        }
+                    }
+                }
+            }
+                .navigationBarTitle("Bookworm")
+                .navigationBarItems(trailing:
+                    Button(action: {
+                        self.showingAddScreen.toggle()
+                    }) {
+                        Image(systemName: "plus")
+                    }
+            ).sheet(isPresented: $showingAddScreen) {
+                AddBookView().environment(\.managedObjectContext, self.moc)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day10/Bookworm/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Tajwar Rahman on 12/31/20.
+//  Copyright © 2020 Tajwar Rahman. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("😴")
+        case 2:
+            return Text("☹️")
+        case 3:
+            return Text("😑")
+        case 4:
+            return Text("😃")
+        default:
+            return Text("🤩")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}

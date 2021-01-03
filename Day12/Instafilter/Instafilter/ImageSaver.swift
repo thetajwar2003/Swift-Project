d088c22b427d23709e48c1d834ee287f2e0e7fc4
thetajwar2003 <rahmant7@bxscience.edu>
2021-01-02 22:48:12 -0500

    Instafilter

---
+++ Day12/Instafilter/Instafilter/ImageSaver.swift
@@ -0,0 +1,26 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Tajwar Rahman on 1/2/21.
+//  Copyright © 2021 Tajwar Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
